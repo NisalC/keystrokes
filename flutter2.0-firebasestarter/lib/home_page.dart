e265c99db5ecbfc0a956884cb18a81936e3b2596
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-11-06 01:15:46 +0530

    floating button

--- flutter2.0-firebasestarter/lib/home_page.dart
+++ flutter2.0-firebasestarter/lib/home_page.dart
@@ -12,15 +12,36 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin {
-
   bool isOpen = false;
   AnimationController _animationController;
   Animation<Color> _buttonColor;
-  Animation<double> animationIcon;
+  Animation<double> _animationIcon;
   Animation<double> _translateButton;
   Curve _curve = Curves.easeOut;
   double _fabHeight = 56.0;
-  
+
+  @override
+  void initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500))
+          ..addListener(() {
+            setState(() {});
+          });
+
+    _animationIcon =
+        Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
+
+    _buttonColor = ColorTween(begin: Colors.green, end: Colors.green[900])
+        .animate(CurvedAnimation(
+            parent: _animationController,
+            curve: Interval(0.00, 1.00, curve: Curves.linear)));
+
+    _translateButton = Tween<double>(begin: _fabHeight, end: -14.0).animate(
+        CurvedAnimation(
+            parent: _animationController,
+            curve: Interval(0.00, 0.75, curve: _curve)));
+  }
+
   @override
   Widget build(BuildContext context) {
     final _profil_name = "Nisal C"; // Replace with backend call
