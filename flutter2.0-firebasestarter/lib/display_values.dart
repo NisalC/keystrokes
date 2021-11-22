595d40813c67c49238e60a34fd9a8c07b2ddc955
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-11-22 21:25:33 +0530

    values displayed

--- flutter2.0-firebasestarter/lib/display_values.dart
+++ flutter2.0-firebasestarter/lib/display_values.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
+import 'package:flutterfbauth/alert_page.dart';
+import 'package:flutterfbauth/services/authservice.dart';
+
 class DisplayValues extends StatefulWidget {
   @override
   _DisplayValuesState createState() => _DisplayValuesState();
@@ -11,5 +14,36 @@ class _DisplayValuesState extends State<DisplayValues>
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('Biometrics'),
+        backgroundColor: Colors.green,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.green,
+              ),
+              child: Text('Nisal C'),
+            ),
+            ListTile(
+              title: const Text('Sign Out'),
+              onTap: () {
+                AuthService().signOut();
+              },
+            ),
+            ListTile(
+              title: const Text('About Us'),
+              onTap: () {
+                MyAlert();
+              },
+            ),
+          ],
+        ),
+      ),
   }
 }
