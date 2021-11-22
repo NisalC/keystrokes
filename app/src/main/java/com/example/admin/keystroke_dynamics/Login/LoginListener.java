2b85e3e4f6e5554de99ef45ccb4a2ef2303eec9c
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-11-22 22:05:37 +0530

    values displaye

--- app/src/main/java/com/example/admin/keystroke_dynamics/Login/LoginListener.java
+++
@@ -1,5 +0,0 @@
-package com.example.admin.keystroke_dynamics.Login;
-
-public interface LoginListener{
-    void onLoginPerformed(Boolean result);
-}
--- app/src/main/java/com/example/admin/keystroke_dynamics/Utils/HashString.java
+++
@@ -1,6 +0,0 @@
-package com.example.admin.keystroke_dynamics.Utils;
-
-public class HashString {
-
-
-}
